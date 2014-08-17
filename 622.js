8c24f7a3d94995be64e99acca79a3198d4df6edf
zzwqq <nsan195@gmail.com>
2014-08-17 12:02:39 +0800

    Create 622.js

---
+++ 622.js
@@ -0,0 +1,5 @@
+$(function(){
+  $("div.article-content-inner").each(function() {
+    $(this).html($(this).html().replace(/9db7a03e8a733563d4c0547de27c5982/gi,"b03552f368c874f4d4c0547de27c5982"));
+  })
+});
